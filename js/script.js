c7d0ed208d70b35c59d0bd10bd7d152a61beb32a
Kholiavskiy Vladimir <vohol@pop-os.localdomain>
2022-02-08 11:47:12 +0200

    fixed title menu, cursor pointer, removed ul from nav, removed fixed at header, fixer mobile menu

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     const HEADER_MENU = document.querySelector('.header__navigation-container');
     const BUTTON_MENU = HEADER_MENU.querySelector('.header__menu-button');
-    console.log(HEADER_MENU);
+    const BUTTON_MENUES = HEADER_MENU.querySelectorAll('.navigation-menu__link');
+
     BUTTON_MENU.addEventListener('click', function () {
         HEADER_MENU.classList.toggle('header__navigation-container--active');
     })
+
+    BUTTON_MENUES.forEach(element => {
+      element.addEventListener('click', function () {
+        HEADER_MENU.classList.remove('header__navigation-container--active');
+      })
+    });
 })
 
-const className = "has-background";
-const scrollTrigger = 60;
+// const className = "has-background";
+// const scrollTrigger = 60;
 
-window.addEventListener('scroll', function(e) {
-  // We add pageYOffset for compatibility with IE.
-  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
-    document.getElementById("header").classList.add(className);
-  } else {
-    document.getElementById("header").classList.remove(className);
-  }
-});
+// window.addEventListener('scroll', function(e) {
+//   // We add pageYOffset for compatibility with IE.
+//   if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+//     document.getElementById("header").classList.add(className);
+//   } else {
+//     document.getElementById("header").classList.remove(className);
+//   }
+// });
